7b96e18764e42c4630ecf3dc1f70f5f5d15cc9e5
Ahmed <ahmedcodes48@gmail.com>
2024-12-26 18:33:41 +0500

    script file with all question added

---
+++ script.js
@@ -0,0 +1,86 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answer: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the national animal of Pakistan?",
+    answer: [
+      {
+        text: "Cat",
+        correct: false,
+      },
+      {
+        text: "Dog",
+        correct: false,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Markhor",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "What is the major color in the flag of Pakistan?",
+    answer: [
+      {
+        text: "Orange",
+        correct: false,
+      },
+      {
+        text: "Red",
+        correct: false,
+      },
+      {
+        text: "Black",
+        correct: false,
+      },
+      {
+        text: "Green",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answer: [
+      {
+        text: "Asia",
+        correct: false,
+      },
+      {
+        text: "Australia",
+        correct: true,
+      },
+      {
+        text: "Arctic",
+        correct: false,
+      },
+      {
+        text: "Africa",
+        correct: false,
+      },
+    ],
+  },
+];
